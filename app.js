d461dbdd39bb5526559d41e6cea3c713987e95f8
Carlo Leonardi <carloleonardi83@gmail.com>
2018-11-26 12:28:04 +0100

    add test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var exp = require('express');
 var bingo = require('bingo-extract');
+var validator = require('validator');
 
 var app = exp();
 // mi ritorna i numeri estratti
@@ -13,14 +14,17 @@ app.get('/reset', function (req, res) {
 })
 // verifico che un numero sia uscito o meno
 app.get('/numbers/:number', function (req, res) {
-    var number=parseInt(req.params.number);
+    var number = validator.toInt(req.params.number);
+    if (validator.isInt(number)) {
+        return res.status(400).json({message: number + ' is not a number'});
+    }
     res.json({extracted:bingo.getExtracted().indexOf(number) >=0});
 })
 // mi ritorna tutti i numeri estratti/non estratti in base a
 // req.query , di default mi ritorna gli estratti
 app.get('/numbers', function (req, res) {
 // se esiste la req.query e se è falso mi ritorna i numeri che non sono ancora usciti
-    if (req.query.extracted && req.query.extracted ==="false"){
+    if (req.query.extracted && validator.isBoolean(req.query.extracted) && !validator.toBoolean(req.query.extracted)){
         return res.json(bingo.getBag());
     }
     // altrimenti se la req.query è true mi ritorna tutti i numeri estratti
